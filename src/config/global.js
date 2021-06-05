eee8147cc418ef56a2f1a2df4c96411f922ff0b3
elrilacho <80546125+elrilacho@users.noreply.github.com>
2021-06-05 11:51:29 -0500

    INFO_TERMINADA

---
+++ src/config/global.js
@@ -0,0 +1,68 @@
+export default {
+  creditos: {
+    liderEquipo: {
+      nombre: 'Maria Camila Garcia Santamaria',
+      cargo: 'Líder del equipo',
+    },
+    contenidoInstruccional: [
+      {
+        nombre: 'Katheryn Dallan Valderrama Varón',
+        cargo: 'Experto Técnico',
+        centro: 'Centro de comercio y servicios. ',
+        regional: 'Regional Tolima',
+      },
+      {
+        nombre: 'José Gregorio Ramírez',
+        cargo: 'Experto Técnico',
+        centro: 'Centro de comercio y servicios.',
+        regional: 'Regional Tolima',
+      },
+      {
+        nombre: 'Bertha Paola Bejarano Avila',
+        cargo: 'Diseñadora instruccional',
+        centro: 'Centro para la Industria de la comunicación ',
+        regional: 'Regional Distrito capital.',
+      },
+    ],
+    desarrolloProducto: [
+      {
+        nombre: 'Francisco José Lizcano Reyes',
+        cargo: 'Responsable del equipo',
+        centro: 'Centro Industrial del Diseño y la Manufactura',
+        regional: 'Regional Santander',
+      },
+      {
+        nombre: 'Leyson Fabian Castaño Perez',
+        cargo: 'Integración de recursos y pruebas',
+        centro: 'Centro Industrial del Diseño y la Manufactura',
+        regional: 'Regional Santander',
+      },
+      {
+        nombre: 'Rafael Mantilla',
+        cargo: 'Evaluadora de contenido y diseño web',
+        centro: 'Centro Industrial del Diseño y la Manufactura',
+        regional: 'Regional Santander',
+      },
+      {
+        nombre: ['Carmen Martínez'],
+        cargo: 'Producción Audiovisual',
+        centro: 'Centro Industrial del Diseño y la Manufactura',
+        regional: 'Regional Santander',
+      },
+      {
+        nombre: 'Camilo Andres Bolaño Rey',
+        cargo: 'Diseño y Desarrollo Front-End',
+        centro: 'Centro Industrial del Diseño y la Manufactura',
+        regional: 'Regional Santander',
+      },
+    ],
+    gestoresRepositorio: [
+      {
+        nombre: 'Milady Tatiana Villamil castellanos',
+        cargo: ['Validación y vinculación en plataforma LMS'],
+        centro: 'Centro de comercio y servicios',
+        regional: 'Regional Tolima',
+      },
+    ],
+  },
+}
---
+++ src/views/Inicio.vue
@@ -0,0 +1,123 @@
+<template lang="pug">
+section
+  .banner-principal.mb-5
+    .container.tarjeta.p-4.p-sm-5(
+      :style="{'background-image': `url(${require('@/assets/curso/fondo-banner-principal.svg')})`}"
+    )
+      .row.justify-content-around.align-items-center
+        .col-lg-7.col-xxl-6.banner-principal__info
+          .banner-principal__programa
+            .h1.mb-0 Procesos para la comercialización internacional 
+          
+          .h2 Información del programa
+          .banner-principal__datos
+            ul
+              li.mb-3.banner-principal__datos__item
+                p.h6.mb-0.me-3.banner-principal__datos__item__titulo Nombre del programa: 
+                p.banner-principal__datos__item__texto Procesos para la comercialización internacional 
+              li.mb-3.banner-principal__datos__item
+                p.h6.mb-0.me-3.banner-principal__datos__item__titulo Código: 
+                p.banner-principal__datos__item__texto 124102
+              li.mb-3.banner-principal__datos__item
+                p.h6.mb-0.me-3.banner-principal__datos__item__titulo Total Horas: 
+                p.banner-principal__datos__item__texto 3984
+              li.mb-3.banner-principal__datos__item
+                p.h6.mb-0.me-3.banner-principal__datos__item__titulo Duración en semanas: 
+                p.banner-principal__datos__item__texto 27 meses
+              li.mb-3.banner-principal__datos__item
+                p.h6.mb-0.me-3.banner-principal__datos__item__titulo Modalidad: 
+                p.banner-principal__datos__item__texto Virtual
+              li.mb-3.banner-principal__datos__item
+                p.h6.mb-0.me-3.banner-principal__datos__item__titulo Requisito de ingreso: 
+                .banner-principal__datos__item__texto
+                  p Superar la prueba de aptitud, motivación, interés y competencias mínimas de ingreso. Además, el aprendiz debe tener dominio de elementos básicos en el manejo de herramientas informáticas y de comunicación como: correo electrónico, chats, procesadores de texto, software para presentaciones, navegadores de internet, otros sistemas y herramientas tecnológicas necesarias para la formación virtual.
+        .d-none.d-lg-block.col-lg-5
+          //- img(src="@/assets/curso/banner-princiapal.svg")
+          .video
+            | <iframe data-v-1f9e4816="" width="560" height="315" src="https://www.youtube.com/embed/2L91WMqw96A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen="allowfullscreen"></iframe>
+
+  .container.tarjeta.tarjeta__template--azul-claro.p-4.p-md-5.mb-5
+    .titulo__template--a.mb-4
+      span.h4 1. Presentación
+
+    p El programa Tecnólogo en Procesos para la comercialización internacional se creó para dar respuesta a la necesidad creciente de contar con talento humano capacitado en inteligencia de mercados que identifique el potencial exportador de la empresa colombiana y gestione clientes potenciales y proveedores internacionales, que beneficien los procesos de comercialización de productos  en las más de 11.000 Pymes  (Portafolio, 2018) y Mipymes  del país, que para el año 2018 fueron de 1.620.342 (Confecámaras, 2018).
+      br
+      br
+      |Así mismo, por medio de este programa se pretende brindar a los diferentes sectores productivos, económicos, sociales, educativos y estatales, la posibilidad de acceder a personal capacitado, con altas aptitudes laborales y profesionales capaces de participar activamente y contribuir al desarrollo económico, tecnológico y social de su entorno y del país.
+
+
+  .container.tarjeta.tarjeta__template--azul-claro.p-4.p-md-5.mb-5
+    .titulo__template--a.mb-4
+      span.h4 2. Justificación<br>del programa
+
+    p El desmayo de la estructura mundial bipolar cambió el panorama económico internacional, remodelándolo conforme a las exigencias de la mundialización de la producción, la integración regional y el florecimiento de la inversión extranjera; generando la construcción de zonas comerciales multinacionales, manifestadas en el multilateralismo global, es decir, normas que regulan la inversión externa y el comercio mundial. Sumado a ello, se tiene la reducción de barreras arancelarias y paraarancelarias en las distintas regiones, ocasionando que las relaciones birregionales dejen de ser simple retórica, para convertirse en una realidad económica concreta.
+      br
+      br
+      |Hoy día, no se concibe un mundo sin tribunas o foros multilaterales, por lo cual, cada país ha hecho lo propio para integrarse a distintas organizaciones con el fin de favorecer el comercio internacional de sus productos y servicios. En el caso de Colombia, los esfuerzos se dirigen principalmente al apoyo para la internacionalización de las PYME, incluyendo medidas específicas relacionadas con la facilitación del comercio, el comercio electrónico, las normas de calidad y la integración regional (OECD/CAF, 2019).
+      br
+      br
+      |Colombia, tiene un número importante de programas de origen estatal y privado, de apoyo a la innovación, incluso para PYMEs y emprendedores, cuyos resultados se publican en línea y funcionan con proyectos promovidos y ejecutados por Instituciones como: Colciencias, INNpulsa, SENA y universidades. Entre los más destacados se encuentran: “Calidad para exportar”, SAFE+, OEA (Operador Económico Autorizado), Sistema Nacional de Competitividad, Ciencia, Tecnología e Innovación (SNCCTl), “Exporta Fácil” y los promovidos por Procolombia para exportadores, entre otros (OECD/CAF, 2019). Como producto de este trabajo conjunto entre instituciones y empresas, tan solo en año 2017, se recibió información de 24.993 nuevas posibilidades de ventas con 7.006 compradores internacionales (Procolombia, 2018). Igualmente, producto de la inversión extrajera, en el año 2017, se generaron 32.507 empleos en los proyectos iniciados (Procolombia, 2018).
+      br
+      br
+      |Finalmente, desde hace cinco décadas, se crearon en Colombia las “Zonas Francas” como instrumentos del comercio exterior y generadoras de empleo. Hoy, en todo el mundo existen 1.245 ZF que han generado 49.292.231 empleos y tienen 516.399 empresas en las actividades comercial e industrial (Portafolio, 2019). Así mismo, las exportaciones de mercancías desde las zonas francas registraron un incremento de 21,5% al pasar de US$225,4 millones FOB en agosto de 2018 a US$273,7 millones FOB en el mismo mes de 2019 (DANE, 2019), múltiples razones por las cuales se hace necesario el desarrollo de la Tecnología en Procesos Para La Negociación Internacional
+
+
+  .container.tarjeta.tarjeta__template--azul-claro.p-4.p-md-5.mb-5
+    .titulo__template--a.mb-4
+      span.h4 3. Competencias<br>a desarrollar
+
+    p 
+
+  .container.tarjeta.tarjeta__template--azul-claro.p-4.p-md-5.mb-5
+    .titulo__template--a.mb-4
+      span.h4 4. Perfil<br>de ingreso
+
+    p Superar la prueba de aptitud, motivación, interés y competencias mínimas de ingreso. Además, el aprendiz debe tener dominio de elementos básicos en el manejo de herramientas informáticas y de comunicación como: correo electrónico, chats, procesadores de texto, software para presentaciones, navegadores de internet, otros sistemas y herramientas tecnológicas necesarias para la formación virtual.
+  .container.tarjeta.tarjeta__template--azul-claro.p-4.p-md-5.mb-5
+    .titulo__template--a.mb-4
+      span.h4 5. Perfil<br>de egreso
+
+    p Asistentes de Mercadeo, Publicidad y Comunicaciones, con 
+      b denominaciones o títulos ocupacionales 
+      b-none en asistencia de mercadeo, publicidad y comunicaciones; apoyando los procesos y actividades de establecimientos y/o departamentos en empresas comerciales e industriales en áreas de Mercadeo, Publicidad y/o Comunicaciones. Están empleados por empresas del sector público y privado.
+  .container.tarjeta.tarjeta__template--azul-claro.p-4.p-md-5.mb-5
+    .titulo__template--a.mb-4
+      span.h4 6. Estrategia<br>metodológica
+
+    p Centrada en la construcción de autonomía para garantizar la calidad de la formación en el marco de la formación por competencias, el aprendizaje por proyectos y el uso de técnicas didácticas activas que estimulan el pensamiento para la resolución de problemas simulados y reales; soportadas en el utilización de las tecnologías de la información y la comunicación, integradas, en ambientes virtuales de aprendizaje, que en todo caso recrean el contexto productivo y vinculan al aprendiz con la realidad cotidiana y el desarrollo de las competencias.
+      br
+      br
+      |Igualmente, debe estimular de manera permanente la autocrítica y la reflexión del aprendiz sobre el que hacer y los resultados de aprendizaje que logra a través de la vinculación activa de las cuatro fuentes de información para la construcción de conocimiento:
+    ul.lista-ul.px-3
+      li
+        i.lista-ul__vineta
+        | El instructor - Tutor
+      li
+        i.lista-ul__vineta
+        | El entorno
+      li
+        i.lista-ul__vineta
+        | las TIC
+      li
+        i.lista-ul__vineta
+        | El trabajo colaborativo
+
+
+  //- Créditos
+  .container.tarjeta.tarjeta__template--azul-claro.p-4.p-md-5.mb-5
+    .titulo__template--a.mb-4
+      span.h4 Créditos
+
+    Creditos
+
+  Footer
+
+</template>
+<script>
+export default {
+  name: 'Inicio',
+  data: () => ({}),
+}
+</script>
+
+<style lang="sass"></style>
