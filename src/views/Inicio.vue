fc8e489f0d725081c1ac1721aff31336f4fca5ce
elrilacho <80546125+elrilacho@users.noreply.github.com>
2021-06-05 19:54:45 -0500

    Update Inicio.vue

--- src/views/Inicio.vue
+++ src/views/Inicio.vue
@@ -34,7 +34,7 @@ section
         .d-none.d-lg-block.col-lg-5
           //- img(src="@/assets/curso/banner-princiapal.svg")
           .video
-            | <iframe data-v-1f9e4816="" width="560" height="315" src="https://www.youtube.com/embed/2L91WMqw96A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen="allowfullscreen"></iframe>
+            | <iframe data-v-1f9e4816="" width="560" height="315" src="https://www.youtube.com/embed/L9qbV3pgl3w" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen="allowfullscreen"></iframe>
 
   .container.tarjeta.tarjeta__template--azul-claro.p-4.p-md-5.mb-5
     .titulo__template--a.mb-4
@@ -48,41 +48,71 @@ section
 
   .container.tarjeta.tarjeta__template--azul-claro.p-4.p-md-5.mb-5
     .titulo__template--a.mb-4
-      span.h4 2. Justificación<br>del programa
-
-    p El desmayo de la estructura mundial bipolar cambió el panorama económico internacional, remodelándolo conforme a las exigencias de la mundialización de la producción, la integración regional y el florecimiento de la inversión extranjera; generando la construcción de zonas comerciales multinacionales, manifestadas en el multilateralismo global, es decir, normas que regulan la inversión externa y el comercio mundial. Sumado a ello, se tiene la reducción de barreras arancelarias y paraarancelarias en las distintas regiones, ocasionando que las relaciones birregionales dejen de ser simple retórica, para convertirse en una realidad económica concreta.
-      br
-      br
-      |Hoy día, no se concibe un mundo sin tribunas o foros multilaterales, por lo cual, cada país ha hecho lo propio para integrarse a distintas organizaciones con el fin de favorecer el comercio internacional de sus productos y servicios. En el caso de Colombia, los esfuerzos se dirigen principalmente al apoyo para la internacionalización de las PYME, incluyendo medidas específicas relacionadas con la facilitación del comercio, el comercio electrónico, las normas de calidad y la integración regional (OECD/CAF, 2019).
-      br
-      br
-      |Colombia, tiene un número importante de programas de origen estatal y privado, de apoyo a la innovación, incluso para PYMEs y emprendedores, cuyos resultados se publican en línea y funcionan con proyectos promovidos y ejecutados por Instituciones como: Colciencias, INNpulsa, SENA y universidades. Entre los más destacados se encuentran: “Calidad para exportar”, SAFE+, OEA (Operador Económico Autorizado), Sistema Nacional de Competitividad, Ciencia, Tecnología e Innovación (SNCCTl), “Exporta Fácil” y los promovidos por Procolombia para exportadores, entre otros (OECD/CAF, 2019). Como producto de este trabajo conjunto entre instituciones y empresas, tan solo en año 2017, se recibió información de 24.993 nuevas posibilidades de ventas con 7.006 compradores internacionales (Procolombia, 2018). Igualmente, producto de la inversión extrajera, en el año 2017, se generaron 32.507 empleos en los proyectos iniciados (Procolombia, 2018).
-      br
-      br
-      |Finalmente, desde hace cinco décadas, se crearon en Colombia las “Zonas Francas” como instrumentos del comercio exterior y generadoras de empleo. Hoy, en todo el mundo existen 1.245 ZF que han generado 49.292.231 empleos y tienen 516.399 empresas en las actividades comercial e industrial (Portafolio, 2019). Así mismo, las exportaciones de mercancías desde las zonas francas registraron un incremento de 21,5% al pasar de US$225,4 millones FOB en agosto de 2018 a US$273,7 millones FOB en el mismo mes de 2019 (DANE, 2019), múltiples razones por las cuales se hace necesario el desarrollo de la Tecnología en Procesos Para La Negociación Internacional
-
-
+      span.h4 2. Competencias<br>a desarrollar
+    ul.lista-ul.px-3
+      li
+        i.lista-ul__vineta
+        | Recolectar información de acuerdo con metodología de investigación
+      li
+        i.lista-ul__vineta
+        | Formular el plan según objetivos y metodologías
+      li
+        i.lista-ul__vineta
+        | Planear la operación de importación y exportación de bienes según normativa de comercio exterior
+      li
+        i.lista-ul__vineta
+        | Orientar investigación formativa según referentes técnicos 
+      li
+        i.lista-ul__vineta
+        | Ejercer derechos fundamentales del trabajo en el marco de la constitución política y los convenios internacionales
+      li
+        i.lista-ul__vineta
+        | Razonar cuantitativamente frente a situaciones susceptibles de ser abordadas de manera matemática en contextos laborales, sociales y personales
+      li
+        i.lista-ul__vineta
+        | Aplicación de conocimientos de las ciencias naturales de acuerdo con situaciones del contexto productivo y social
+      li
+        i.lista-ul__vineta
+        | Utilizar herramientas informáticas de acuerdo con las necesidades de manejo de información
+      li
+        i.lista-ul__vineta
+        | Desarrollar procesos de comunicación eficaces y efectivos, teniendo en cuenta situaciones de orden social, personal y productivo
+      li
+        i.lista-ul__vineta
+        | Interactuar en lengua inglesa de forma oral y escrita dentro de contextos sociales y laborales según los criterios establecidos por el Marco Común Europeo de referencia para las lenguas
+      li
+        i.lista-ul__vineta
+        | Desarrollar la operación de importación y exportación de mercancías según normativa de comercio exterior
+      li
+        i.lista-ul__vineta
+        | Negociar portafolio de acuerdo con categoría de producto o servicio.
+      li
+        i.lista-ul__vineta
+        | Generar hábitos saludables de vida mediante la aplicación de programas de actividad física en los contextos productivos y sociales
+      li
+        i.lista-ul__vineta
+        | Gestionar procesos propios de la cultura emprendedora y empresarial de acuerdo con el perfil personal y los requerimientos productivo y social
+      li
+        i.lista-ul__vineta
+        | Aplicar prácticas de protección ambiental, seguridad y salud en el trabajo de acuerdo con las políticas organizacionales y la normatividad vigente
+      li
+        i.lista-ul__vineta
+        | Interactuar en el contexto productivo y social de acuerdo con principios éticos para la construcción de una cultura de paz
   .container.tarjeta.tarjeta__template--azul-claro.p-4.p-md-5.mb-5
     .titulo__template--a.mb-4
-      span.h4 3. Competencias<br>a desarrollar
-
-    p 
-
-  .container.tarjeta.tarjeta__template--azul-claro.p-4.p-md-5.mb-5
-    .titulo__template--a.mb-4
-      span.h4 4. Perfil<br>de ingreso
+      span.h4 3. Perfil<br>de ingreso
 
     p Superar la prueba de aptitud, motivación, interés y competencias mínimas de ingreso. Además, el aprendiz debe tener dominio de elementos básicos en el manejo de herramientas informáticas y de comunicación como: correo electrónico, chats, procesadores de texto, software para presentaciones, navegadores de internet, otros sistemas y herramientas tecnológicas necesarias para la formación virtual.
   .container.tarjeta.tarjeta__template--azul-claro.p-4.p-md-5.mb-5
     .titulo__template--a.mb-4
-      span.h4 5. Perfil<br>de egreso
+      span.h4 4. Perfil<br>de egreso
 
     p Asistentes de Mercadeo, Publicidad y Comunicaciones, con 
       b denominaciones o títulos ocupacionales 
       b-none en asistencia de mercadeo, publicidad y comunicaciones; apoyando los procesos y actividades de establecimientos y/o departamentos en empresas comerciales e industriales en áreas de Mercadeo, Publicidad y/o Comunicaciones. Están empleados por empresas del sector público y privado.
   .container.tarjeta.tarjeta__template--azul-claro.p-4.p-md-5.mb-5
     .titulo__template--a.mb-4
-      span.h4 6. Estrategia<br>metodológica
+      span.h4 5. Estrategia<br>metodológica
 
     p Centrada en la construcción de autonomía para garantizar la calidad de la formación en el marco de la formación por competencias, el aprendizaje por proyectos y el uso de técnicas didácticas activas que estimulan el pensamiento para la resolución de problemas simulados y reales; soportadas en el utilización de las tecnologías de la información y la comunicación, integradas, en ambientes virtuales de aprendizaje, que en todo caso recrean el contexto productivo y vinculan al aprendiz con la realidad cotidiana y el desarrollo de las competencias.
       br
